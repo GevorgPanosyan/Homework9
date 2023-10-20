08aec7ee24a5052f1b57ac44071d29dfba888191
Gevorg <117856136+GevorgPanosyan@users.noreply.github.com>
2023-10-20 16:22:26 +0300

    Add files via upload

---
+++ Program66.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+ натуральных элементов в промежутке от M до N.*/
+
+class Program {
+  static void Main (string[] args) {
+    Console.WriteLine("Введите M:");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите N:");
+    int N = Convert.ToInt32(Console.ReadLine());
+
+    int sum = 0;
+    int number = 1;
+    while (number <= N) {
+      sum += number;
+      number++;
+    }
+    Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна {sum}");
+  }
+}
